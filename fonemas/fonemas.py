db0a67b682a06244c27925b9cb8a27a57b27aab5
Fernando <fernando@sanz-lazaro.com>
2021-10-28 14:04:53 +0200

    correcciones

--- fonemas/fonemas.py
+++ fonemas/fonemas.py
@@ -5,11 +5,11 @@ import silabeador
 
 
 consonantes = {'w': 'b', 'v': 'b', 'z': 'θ', 'x': 'ks', 'j': 'x', 'ch':'tʃ',
-               'ñ': 'ɲ', 'y': 'j'}
-dobles = {'qu': 'k', 'll': 'ʎ', 'ch':'tʃ',  'r': 'ɾ', 'ɾɾ': 'r',
+               'ñ': 'ɲ', 'y': 'j',
+               'qu': 'k', 'll': 'ʎ', 'ch':'tʃ',  'r': 'ɾ', 'ɾɾ': 'r',
           'sɾ': 'sr', 'lɾ': 'lr', 'nɾ': 'nr',
           'ce': 'θe', 'cé': 'θe', 'cë': 'θe',
-          'ci': 'θi', 'cí': 'θi', 'cï': 'θ', 'c': 'k'}
+               'ci': 'θi', 'cí': 'θi', 'cï': 'θ', 'c': 'k', 'h':''}
 diacriticos = {'á': 'a', 'à': 'a', 'ä': 'a',
                'é': 'e', 'è': 'e', 'ë': 'e',
                'í': 'i', 'ì': 'i', 'ï': 'i',
@@ -21,26 +21,19 @@ diacriticos = {'á': 'a', 'à': 'a', 'ä': 'a',
 
 def transcribe(palabra):
     palabra = palabra.lower()
-    for unica in consonantes:
-        if unica in palabra:
-            palabra = palabra.replace(unica, consonantes[unica])
-    for doble in dobles:
-        if doble in palabra:
-            palabra = palabra.replace(doble, dobles[doble])
-    if palabra.startswith('ɾ'):
-        palabra = re.sub('^ɾ', 'r', palabra)
-    #if re.search('[nls]ɾ', palabra):
-    #    re.sub('([nls])ɾ','\1r', palabra)
+    silabas_des = silabeador.silabas(palabra)
+    silabas = silabas_des.silabas
+    for idx, silaba in enumerate(silabas):
+        for letras in consonantes:
+            if letras in silaba:
+                silabas[idx]  = silaba.replace(letras, consonantes[letras])
+    if palabra[0].startswith('ɾ'):
+        palabra[0] = re.sub('^ɾ', 'r', palabra[0])
     if 'g' in palabra:
-        for reg in [[r'g([eiEIéíÉÍËÏëï])', rf'x\1'],
-                    [r'g[uU]([eiEIéíÉÍËÏëï])', rf'g\1']]:
-            palabra = re.sub(reg[0], reg[1], palabra)
-    if 'c' in palabra:
-        for reg in [[r'c([eiEIéíÉÍËÏëï])', rf'θ\1'],
-                    [r'c([aouAOUÁÓÚáóúäöüÄÖÜ])', rf'k\1']]:
+        for reg in [[r'g([eiéíiëï])', rf'x\1'],
+                    [r'g[u]([eiéíëï])', rf'g\1']]:
             palabra = re.sub(reg[0], reg[1], palabra)
     silabas_des =  silabeador.silabas(palabra)
-    silabas = silabas_des.silabas
     silabas[silabas_des.tonica] = f"'{silabas[silabas_des.tonica]}"
     if any('h' in silaba for silaba in silabas):
         silabas = [silaba.replace('h', '') for silaba in silabas]
