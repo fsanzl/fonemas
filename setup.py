2e10dcdaad64fe88b7ddb1f1d47e6d39e99f008b
Fernando <fernando@sanz-lazaro.com>
2022-03-22 12:10:42 +0100

    version bump

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = ['silabeador']
 # This call to setup() does all the work
 setup(
     name="fonemas",
-    version="2.0.0-2",
+    version="2.0.0-3",
     python_requires='>=3.5',
     description="Phonetic transcription of Spanish",
     long_description=README,
