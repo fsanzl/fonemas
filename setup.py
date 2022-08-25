2c18399f0810decab046f90aa3a22f4f7530b06b
Fernando Sanz <fernando@sanz-lazaro.com>
2022-08-25 21:33:21 +0200

    requires

--- setup.py
+++ setup.py
@@ -5,43 +5,43 @@ from setuptools import setup
 HERE = pathlib.Path(__file__).parent
 
 # The text of the README file
-README = (HERE / "README.md").read_text()
+README = (HERE / 'README.md').read_text()
 
 install_requires = ['silabeador']
 
 
 # This call to setup() does all the work
 setup(
-    name="fonemas",
-    version="2.0.15",
+    name='fonemas',
+    version='2.0.15',
     python_requires='>=3.5',
-    description="Phonetic transcription of Spanish",
+    description='Phonetic transcription of Spanish',
     long_description=README,
-    long_description_content_type="text/markdown",
-    url="https://github.com/fsanzl/fonemas",
+    long_description_content_type='text/markdown',
+    url='https://github.com/fsanzl/fonemas',
     project_urls={
         'Source': 'https://github.com/fsanzl/fonemas/',
         'Tracker': 'https://github.com/fsanzl/fonemas/issues',
     },
-    author="Fernando Sanz-Lázaro",
-    author_email="fsanzl@gmail.com",
-    license="LGPL",
+    author='Fernando Sanz-Lázaro',
+    author_email='fsanzl@gmail.com',
+    license='LGPL',
     classifiers=[
-        "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.5",
-        "Natural Language :: Spanish",
+        'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.5',
+        'Natural Language :: Spanish',
     ],
-    packages=["fonemas"],
-    setup(install_requires=['silabeador'], ...)
+    packages=['fonemas'],
+    install_requires=['silabeador'],
     include_package_data=True,
     entry_points={
-        "console_scripts": [
-            "fonemas=fonemas.__main__:main",
+        'console_scripts': [
+            'fonemas=fonemas.__main__:main',
         ]
     },
 )
